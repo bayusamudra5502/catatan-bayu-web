d09a4a356434013997ef1618808f08c4d6dfa99d
bayusamudra5502 <bayusamudra.55.02.com@gmail.com>
2022-08-29 12:40:18 +0700

    options

--- gatsby-config.js
+++ gatsby-config.js
@@ -63,33 +63,35 @@ module.exports = {
       resolve: `gatsby-plugin-mdx`,
       options: {
         extensions: [`.mdx`],
-        gatsbyRemarkPlugins: [
-          {
-            resolve: `gatsby-remark-katex`,
-          },
-          {
-            resolve: `gatsby-remark-images`,
-            options: {
-              maxWidth: 630,
+        mdxOptions: {
+          gatsbyRemarkPlugins: [
+            {
+              resolve: `gatsby-remark-katex`,
             },
-          },
-          {
-            resolve: `gatsby-remark-responsive-iframe`,
-            options: {
-              wrapperStyle: `margin-bottom: 1.0725rem`,
+            {
+              resolve: `gatsby-remark-images`,
+              options: {
+                maxWidth: 630,
+              },
             },
-          },
-          {
-            resolve: `gatsby-remark-prismjs`,
-            options: {
-              showLineNumbers: false
-            }
-          },
+            {
+              resolve: `gatsby-remark-responsive-iframe`,
+              options: {
+                wrapperStyle: `margin-bottom: 1.0725rem`,
+              },
+            },
+            {
+              resolve: `gatsby-remark-prismjs`,
+              options: {
+                showLineNumbers: false
+              }
+            },
 
-          `gatsby-remark-copy-linked-files`,
-          `gatsby-remark-smartypants`,
-        ],
-      },
+            `gatsby-remark-copy-linked-files`,
+            `gatsby-remark-smartypants`,
+          ],
+        },
+      }
     },
     `gatsby-transformer-sharp`,
     `gatsby-plugin-sass`,
